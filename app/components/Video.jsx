e316213e344f8221a848685ebf26bdfc1013f6f9
No_otra_vez <saulbector@gmail.com>
2024-01-05 17:08:58 -0600

    Fix portrait mode

--- app/components/Video.jsx
+++ app/components/Video.jsx
@@ -2,7 +2,7 @@ export default function Video() {
     return (
         <div className="w-full relative">
             <video
-                className="absolute w-full h-screen -z-40 object-cover object-[-275px_0px] md:object-center"
+                className="absolute w-full h-screen -z-40 object-cover portrait:object-[-275px_0px] md:object-center"
                 src="./web.mp4" autoPlay loop muted style={{ clipPath: 'polygon(0 0, 100% 0, 100% 99.9%, 0 99.9%)' }}
                 >
             </video>
