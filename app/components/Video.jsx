631fdc409f6adcff78d889e0f2409ec9c8fe6659
No_otra_vez <saulbector@gmail.com>
2024-01-05 18:09:13 -0600

    play video on mobile

--- app/components/Video.jsx
+++ app/components/Video.jsx
@@ -3,7 +3,7 @@ export default function Video() {
         <div className="w-full relative">
             <video
                 className="absolute w-full h-screen -z-40 object-cover portrait:object-[-275px_0px] md:object-center"
-                src="./web.mp4" autoPlay loop muted style={{ clipPath: 'polygon(0 0, 100% 0, 100% 99.9%, 0 99.9%)' }}
+                src="./web.mp4" autoPlay loop playsInline muted style={{ clipPath: 'polygon(0 0, 100% 0, 100% 99.9%, 0 99.9%)' }}
                 >
             </video>
         </div>
