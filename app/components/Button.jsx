70a939ef503650c614cb213d1fe46d3f9a52c6ff
Sbector <saulbector@gmail.com>
2024-07-08 12:30:35 -0600

    Change button style

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -3,7 +3,12 @@ import Link from "next/link";
 export default function Button({ href, children, className, target }) {
     return (
 
-        <Link href={href} target={target} className={`w-auto bg-gray-800 hover:bg-green-500 px-2 py-1 ${className}`}>
+        <Link href={href} 
+        target={target} 
+        className={`w-auto bg-gray-800 
+        hover:bg-green-500 hover:text-black 
+        px-2 py-1 
+        ${className}`}>
             <button>
                 {children}
             </button>
